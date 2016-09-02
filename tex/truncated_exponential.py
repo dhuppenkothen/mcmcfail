cba60d93b73d81f87482c224a051a0664ba859ca
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-09-02 12:31:26 +1200

    Bugfix, nice dataset

--- tex/truncated_exponential.py
+++ tex/truncated_exponential.py
@@ -6,9 +6,9 @@ rng.seed(0)
 
 # GENERATE A DATASET
 
-L = 47.34                   # True value of L
-N = 5                       # Number of data points
-t_min, t_max = 5.0, 20.0    
+L = 10.3                     # True value of L
+N = 100                        # Number of data points
+t_min, t_max = 0.0, 15.0    
 t = []
 for i in range(0, N):
     while True:
@@ -21,16 +21,14 @@ t = np.array(t)
 
 plt.hist(t, bins=100, color="k", alpha=0.1)
 plt.xlabel("$t$", fontsize=16)
-plt.xlim([0.0, 30.0])
-plt.ylim([0, 3])
 plt.title("Photon Arrival Times")
 plt.show()
 
 # TO INFER L from {t}, need the likelihood. Here it is.
 # Not using log-likelihood, because N isn't too big.
-L = np.linspace(0.0, 1000.0, 10001)
+L = np.linspace(0.0, 100.0, 10001)
 likelihood = L**(-N)\
-                *(np.exp(-t_min/L) - np.exp(-t_max/L))\
+                *(np.exp(-t_min/L) - np.exp(-t_max/L))**(-N)\
                 *np.exp(-t.sum()/L)
 
 plt.plot(L, likelihood, "k")
